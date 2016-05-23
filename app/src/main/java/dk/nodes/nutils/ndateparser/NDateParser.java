2e9b6ed6ba6cc6b318616a246d20437616613524
Constantin Teodor Gherghescu <Constantin Teodor Gherghescu>
2016-05-23 13:36:23 +0200

    added initial date parsing methods

---
+++ app/src/main/java/dk/nodes/nutils/ndateparser/NDateParser.java
@@ -0,0 +1,37 @@
+package dk.nodes.nutils.ndateparser;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by Constantin Gherghescu on 23-May-16.
+ */
+public class NDateParser {
+
+
+    /**
+     * Tries to parse an ISO 8601 string representation of a date/time
+     *
+     * @param dateString the string containing the date: "yyyy-MM-dd'T'HH:mm:ssZ"
+     * @return the date represented by {@code dateString}
+     */
+    public static Date ParseDateFromISOString(String dateString) throws ParseException {
+        Date date = (new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US)).parse(dateString);
+        return date;
+    }
+
+    /**
+     * Tries to convert a {@code Date} object to an ISO 8601 string representation
+     *
+     * @param date A {@code Date} instance
+     * @return ISO 8601 string representation: "yyyy-MM-dd'T'HH:mm:ssZ"
+     */
+    public static String DateToISOString(Date date) {
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US);
+        return df.format(date);
+    }
+}
+
